1951704dd27879c7b9e539a89266188ea1cac8bf
avsoto <avsotomejia@gmail.com>
2025-01-17 01:16:46 -0500

    add: new invoice component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InvoiceComponent } from './components/invoice/invoice.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [InvoiceComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
