864412ad8e4c6fbafa8ac79c7b2173f18703826e
avsoto <avsotomejia@gmail.com>
2025-01-18 14:23:26 -0500

    add: [components] generate components

---
+++ src/app/components/invoice-view/invoice-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invoice-view',
+  imports: [],
+  templateUrl: './invoice-view.component.html'
+})
+export class InvoiceViewComponent {
+
+}
---
+++ src/app/components/row-item/row-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-row-item',
+  imports: [],
+  templateUrl: './row-item.component.html'
+})
+export class RowItemComponent {
+
+}
