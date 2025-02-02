a123b53c5fbf0848baaaf3874f26b92e89126ac6
avsoto <avsotomejia@gmail.com>
2025-02-02 17:30:19 -0500

    add [form-item] implementing form to save new items to the invoice

--- src/app/components/invoice/invoice.component.ts
+++ src/app/components/invoice/invoice.component.ts
@@ -6,10 +6,18 @@ import { ClientViewComponent } from '../client-view/client-view.component';
 import { CompanyViewComponent } from '../company-view/company-view.component';
 import { ListItemsComponent } from '../list-items/list-items.component';
 import { RowItemComponent } from '../row-item/row-item.component';
+import { TotalComponent } from '../total/total.component';
+import { FormItemComponent } from '../form-item/form-item.component';
+import { Item } from '../../models/item';
 
 @Component({
   selector: 'app-invoice',
-  imports: [InvoiceViewComponent, ClientViewComponent, CompanyViewComponent, ListItemsComponent, RowItemComponent],
+  imports: [InvoiceViewComponent, 
+    ClientViewComponent, 
+    CompanyViewComponent, 
+    ListItemsComponent,
+    TotalComponent,
+    FormItemComponent],
   templateUrl: './invoice.component.html',
 })
 export class InvoiceComponent implements OnInit{
@@ -20,5 +28,13 @@ export class InvoiceComponent implements OnInit{
   
   ngOnInit(): void {
     this.invoice = this.service.getInvoice();
+  }
+
+  removeItem(id: number){
+    this.invoice = this.service.remove(id);
+  }
+
+  addItem(item: Item){
+    this.invoice = this.service.save(item);
   }
 }
